7c44618595c027ce87165482332c85b74e9d9fc3
temogogitidze <temogogitidze@redberry.ge>
2023-12-27 15:57:26 +0400

    feat: add sidebar links and style

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 const LeftSidebar = () => {
+    const { pathname } = useLocation();
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+    const navigate = useNavigate();
+    const { user } = useUserContext();
+
+    useEffect(() => {
+        if (isSuccess) navigate(0);
+    }, [isSuccess]);
     return (
         <nav className="leftsidebar">
             <div className="flex flex-col gap-11">
@@ -9,10 +23,55 @@ const LeftSidebar = () => {
                     <img
                         src="assets/images/logo.svg"
                         alt="logo"
-                        width={130}
-                        height={325}
+                        width={170}
+                        height={36}
                     />
                 </Link>
+
+                <Link to={`/profile/`} className="flex gap-3 items-center">
+                    <img
+                        className="h-14 w-14 rounded-full"
+                        src={
+                            user.imageUrl ||
+                            "/assets/icons/profile-placeholder.svg"
+                        }
+                        alt="profile"
+                    />
+                    <div className="flex flex-col">
+                        <p className="body-bold">{user.name}</p>
+                        <p className="small-regular text-light-3">
+                            @{user.username}
+                        </p>
+                    </div>
+                </Link>
+
+                <ul className="flex flex-col gap-6">
+                    {sidebarLinks.map((link: INavLink) => {
+                        const isActive = pathname === link.route;
+                        return (
+                            <li
+                                className={`leftsidebar-link group ${
+                                    isActive && "bg-primary-500"
+                                }`}
+                                key={link.label}
+                            >
+                                <NavLink
+                                    className="flex gap-4 items-center p-4"
+                                    to={link.route}
+                                >
+                                    <img
+                                        src={link.imgURL}
+                                        alt={link.label}
+                                        className={`group-hover:invert-white ${
+                                            isActive && "invert-white"
+                                        }`}
+                                    />
+                                    {link.label}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         </nav>
     );
